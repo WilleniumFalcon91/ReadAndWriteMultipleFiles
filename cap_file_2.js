e7b9e927a3281c2f8c6b9f1c2216b9802c48e603
William Elliott <wcelliott1991@gmail.com>
2017-08-03 12:10:53 -0400

    fixes bug typo

---
+++ cap_file_2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const readline = require('readline')
+
+const rl = readline.createInterface( {
+    input: process.stdin, 
+    output: process.stdout
+})
+
+rl.question('filename: ', (input) =>  {
+    rl.question('filename: ', (output) => {
+        rl.close();
+        fs.readline(input, (err, buffer) => {
+            if (err) {
+                console.log(err.message);
+                return;
+            } 
+            let content = buffer.toString();
+            let upcase = content.toUpperCase();
+            fs.writeFile(output, upcase, (err) => {
+                console.log('It worked!');
+            })
+        })
+    })
+})
